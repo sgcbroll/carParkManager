9ae00dfaeb4026b261ff46ad9d0eb54466b916c1
sgcbroll <101812982+sgcbroll@users.noreply.github.com>
2022-03-28 13:05:56 +0100

    Update carparkmanager.sh

--- carparkmanager.sh
+++ carparkmanager.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "Please enter name for raspberry pi"
+read piname
+echo "Is this an entry or exit sensor?"
 lxterminal -e sudo python3 QRreader.py $1
 lxterminal -e sudo python3 RFIDreader.py $1
