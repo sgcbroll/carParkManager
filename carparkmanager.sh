c5c1d2d5e813bdee2be54eba79bbd83e3dcca390
sgcbroll <101812982+sgcbroll@users.noreply.github.com>
2022-03-28 13:55:29 +0100

    Update carparkmanager.sh

--- carparkmanager.sh
+++ carparkmanager.sh
@@ -5,3 +5,4 @@ echo "Is this an entry or exit sensor?"
 read pi_usage
 lxterminal -e sudo python3 QRreader.py $pi_name $pi_usage $1
 lxterminal -e sudo python3 RFIDreader.py $pi_name $pi_usage $1
+sudo python3 exitButton.py $pi_name $pi_usage $1
